c450b488d81f51be73f359d41d45470c380b14cd
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-12-26 16:01:32 +0000

    built basic UI

---
+++ script.js
@@ -0,0 +1,8 @@
+const userInputHours = prompt("How many hours does this food item need?");
+const userInputMinutes = prompt("How many minutes does this food item need?");
+const timerHoursDisplay = document.querySelector(".timer-hours");
+const timerMinutesDisplay = document.querySelector(".timer-minutes");
+const timerSecondsDisplay = document.querySelector(".timer-seconds");
+timerHoursDisplay.textContent = userInputHours;
+timerMinutesDisplay.textContent = userInputMinutes;
+1;
