10397228eadb5a9afdab9254a80e3f62bf60ae51
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-12-27 21:27:58 +0000

    added user input functionality

--- script.js
+++ script.js
@@ -3,78 +3,98 @@ const timerMinutesDisplay = document.querySelector(".timer-minutes");
 const timerSecondsDisplay = document.querySelector(".timer-seconds");
 const startBtn = document.querySelector(".start-btn");
 const foodItems = document.querySelector(".food-items");
+// const inputFoodName = document.querySelector("#foodname").value;
+// const inputHours = document.querySelector("#hours").value;
+// const inputMinutes = document.querySelector("#minutes").value;
+const form = document.querySelector(".form");
+let foodItemsObject = {};
 
-const foodItemsObject = {
-  gammon: {
-    name: "gammon",
-    h: 0,
-    m: 1,
-    s: 12,
-  },
+// to capture the input from user
+// and store it in the foodItemsObject
 
-  veg: {
-    name: "veg",
-    h: 0,
-    m: 02,
-    s: 00,
-  },
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputFoodName = document.querySelector("#foodname").value;
 
-  potatoes: {
-    name: "potatoes",
-    h: 12,
-    m: 0,
-    s: 30,
-  },
-};
+  const inputHours = document.querySelector("#hours").value;
+  const inputMinutes = document.querySelector("#minutes").value;
+  const inputSeconds = document.querySelector("#seconds").value;
+
+  foodItemsObject[inputFoodName] = {
+    name: `${inputFoodName}` || "❓❓❓",
+    h: inputHours || 0,
+    m: inputMinutes || 0,
+    s: inputSeconds || 0,
+  };
+
+  displayFoodItems(foodItemsObject[inputFoodName]);
+  form.reset();
+  const startTimerButtons = document.querySelectorAll(".start-btn");
+  console.log(startTimerButtons);
+  startTimerButtons.forEach(function (e) {
+    // console.log(element.parentNode.firstElementChild.textContent);
+    e.addEventListener("click", function () {
+      timerFunction(e);
+    });
+  });
+});
+// end of input capture functionality
 
 // to display each food item in a row
 function displayFoodItems(foodItem) {
   const hours = (`0` + `${String(foodItem.h)}`).slice(-2);
   const minutes = (`0` + `${String(foodItem.m)}`).slice(-2);
   const secs = (`0` + `${String(foodItem.s)}`).slice(-2);
+
+  // create the HTML for each item row
   const html = `
   <div class="timer-row">
         
   <div class="food-name">${foodItem.name}</div>
   <div class="food-timer">
-    <p class="timer-hours timer-display">${hours}&nbsp;&nbsp;</p>
-    <p class="timer-separator">:&nbsp;</p>
-    <p class="timer-minutes timer-display">${minutes}&nbsp;&nbsp;</p>
-    <p class="timer-separator">:&nbsp;</p>
+    <p class="timer-hours timer-display">${hours}</p>
+    <p class="timer-separator">&nbsp;&nbsp;:&nbsp;</p>
+    <p class="timer-minutes timer-display">${minutes}</p>
+    <p class="timer-separator">&nbsp;&nbsp;:&nbsp;</p>
     <p class="timer-seconds timer-display">${secs}</p>
   </div>
   <div class="start-btn"><i class="fas fa-play"></i></div>
   
 </div>
-<div class="progress-bar"></div>
+<div class="progress-bar"> <p class="ready-message hidden">Ready!</p> </div>
     `;
 
+  // insert the HTML into the app window
   foodItems.insertAdjacentHTML("beforeend", html);
-  foodItem.startTimerBtn = document.querySelector(".start-btn");
 }
+// end of input display functionality
 
-displayFoodItems(foodItemsObject.gammon);
-displayFoodItems(foodItemsObject.veg);
-displayFoodItems(foodItemsObject.potatoes);
+// timer functionality
 
-const startTimerButtons = document.querySelectorAll(".start-btn");
+const readyMessage = document.querySelector(".ready-message");
 
-startTimerButtons.forEach(function (e) {
-  e.addEventListener("click", function () {
-    e.classList.add("hidden");
-    let seconds =
-      Number(e.previousElementSibling.firstElementChild.textContent) * 3600 +
-      Number(
-        e.previousElementSibling.firstElementChild.nextElementSibling
-          .nextElementSibling.textContent
-      ) *
-        60 +
-      Number(e.previousElementSibling.lastElementChild.textContent);
-    e.parentElement.nextElementSibling.style.width = "100%";
-    e.parentElement.nextElementSibling.style.transition = `all ${seconds}s`;
-    setInterval(function () {
+function timerFunction(e) {
+  e.classList.add("hidden");
+  let seconds =
+    Number(e.previousElementSibling.firstElementChild.textContent) * 3600 +
+    Number(
+      e.previousElementSibling.firstElementChild.nextElementSibling
+        .nextElementSibling.textContent
+    ) *
+      60 +
+    Number(e.previousElementSibling.lastElementChild.textContent);
+  e.parentElement.nextElementSibling.style.width = "100%";
+  e.parentElement.nextElementSibling.style.transition = `all ${seconds}s`;
+
+  setInterval(function () {
+    if (seconds === 0) {
+      clearInterval();
+      e.parentElement.classList.add("food-ready");
+      e.parentElement.nextElementSibling.firstElementChild.classList.remove(
+        "hidden"
+      );
+    } else {
       seconds--;
-
       const newSeconds = seconds % 60;
       const newHours = Math.trunc(seconds / 3600);
       const newMinutes = Math.trunc(seconds / 60) % 60;
@@ -86,6 +106,6 @@ startTimerButtons.forEach(function (e) {
       e.previousElementSibling.firstElementChild.textContent = (
         0 + `${String(newHours)}`
       ).slice(-2);
-    }, 1000);
-  });
-});
+    }
+  }, 1000);
+}
